f043354166eda635f38cd059216edf8092229095
faldea1 <faldea1@uc.cl>
2022-12-17 20:11:22 +0000

    React Project Start

---
+++ src/components/counter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { } from 'react-icons/fa';
+
+const TheCounter = () => {
+	return (
+		<div>dsd</div>
+	);
+};
+
+export default TheCounter;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Agregar bootstrap desde node_modules
+// IMP bootstrap desde node_modules
 import 'bootstrap/dist/css/bootstrap.min.css'; // Estilos de Bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle'; // Javascript de Bootstrap
 
-// Agregar font-awesome
+// IMP font-awesome
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import App from './App'
 
-ReactDOM.createRoot(document.querySelector('#root')).render(<App />);
+//COMPONENTE VIEW
+import TheCounter from "./components/counter.jsx"
+
+ReactDOM.createRoot(document.querySelector('#root')).render(<TheCounter />);
