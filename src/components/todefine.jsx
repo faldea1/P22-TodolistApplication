429a38fbff8a0a907f0513f5d2c0b7cff9dfc57b
faldea1 <faldea1@uc.cl>
2022-12-27 17:14:25 +0000

    Todolist table

--- src/components/todefine.jsx
+++ src/components/todefine.jsx
@@ -1,10 +1,22 @@
 import React, { useRef, useState, useEffect } from "react";
-import { FaPlay } from 'react-icons/fa';
+import { FaPlay, FaTasks } from 'react-icons/fa';
 
 
 const ToDefine = () => {
 	return (
-	<div><FaPlay /></div>
+	<div className="container">
+		<h1>My To Do List <span><FaTasks /></span></h1>
+		<ul>
+			<li><input type="text" placeholder="Add a morning action to start a good day"></input></li>
+			<li>Make the bed</li>
+			<li>Sport</li>
+			<li>Take a Shower</li>
+			<li>Breakfast</li>
+			<li>Brush Teeths</li>
+			<li>Start the day with a Coffe</li>	
+		</ul>
+		<div>Number of Actions</div>
+	</div>
 	);
 };
 
